c0a7b52542261aec5c1ec8fd4344bfbf8c56adf2
Kolejarzyk <pawel.dee@live.com>
2018-01-04 12:50:19 +0100

    KOLEJARZ/TASK12 Register,Login and Authenticatio script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,13 @@ import { MatchComponent } from './match/match.component';
 import { StreamsComponent } from './streams/streams.component';
 import { HttpModule } from '@angular/http';
 import { MainAppComponent } from './main-app/main-app.component';
-
+import { AlertComponent } from './-alert/-alert.component';
+import { AppConfig } from './app.config';
+import { AuthGuard } from './.guard/auth.guard';
+import { AlertService } from './.services/alert.service';
+import { AuthenticationService } from './.services/authentication.service';
+import { UserService } from './.services/user.service';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,14 +39,23 @@ import { MainAppComponent } from './main-app/main-app.component';
     MatchComponent,
     StreamsComponent,
     MainAppComponent,
+    AlertComponent,
   ],
   imports: [
     BrowserModule, 
     HttpModule,
     NgxCarouselModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    AppConfig,
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
