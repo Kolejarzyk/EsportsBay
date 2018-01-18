f8e3fc53c18ff7d2fe231713bdbf554b98a2196e
Kolejarzyk <pawel.dee@live.com>
2018-01-18 18:14:56 +0100

    fix display inline stream

--- src/app/streams/StreamModel.ts
+++ src/app/streams/StreamModel.ts
@@ -3,14 +3,16 @@ export class StreamModel
     game: string;
     display_name: string;
     language: string;
+    imgUrl : string;
     url: string;
 
 
-    constructor(game: string, display_name: string, language: string, url: string)
+    constructor(game: string, display_name: string, language: string,imgUrl:string, url: string)
     {
         this.game = game;   
         this.display_name = display_name;
         this.language = language;
+        this.imgUrl = imgUrl;
         this.url = url;
     }
 }
